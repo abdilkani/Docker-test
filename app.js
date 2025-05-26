79fabd94a17b84bc0f7d1ee4f8f89ea05d1d2880
Abdilkani Eidle <ae@Abdilkanis-MacBook-Air.local>
2025-05-26 10:56:13 +0200

    V2

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is ready on port ${PORT}`);
+// });
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is ready on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
